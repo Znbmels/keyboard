eda9b74066acd773cbd10f60b80ed291d6d6e504
Your Name <your.email@example.com>
2025-06-19 11:52:02 +0500

    Initial Commit

---
+++ Keyboard/KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardApp.swift
+//  Keyboard
+//
+//  Created by Zainab on 19.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
